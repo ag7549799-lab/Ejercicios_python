599f1f7714cb81af31ee0a06071d4761251a876c
Andrea <ag7549799@gmail.com>
2026-01-28 22:45:10 -0600

    Primer commit

---
+++ condicionales/condicionales/condicionales.py
@@ -0,0 +1,31 @@
+#Condicion con Pynthon
+#if,else,elif
+
+"""
+if expo_bool:
+	instrucciones
+if expo_bool:
+	instrucciones
+else:
+	instrucciones
+
+	if expo_bool
+	instrucciones
+elif expo_bool:
+	instrucciones
+	else:
+	instrucciones
+
+"""
+print('Inicio')
+
+numero = int(input('Ingresa un numero:'))
+
+if numero > 0:
+	print('Es un numero Positivo')
+elif numero==0:
+	print('Es Cero')
+else:
+	print('Es un numero Negativo')
+	
+print('Fin')		
