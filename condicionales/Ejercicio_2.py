47d92c38890e96c16538763320f2cb8887508eab
Andrea <ag7549799@gmail.com>
2026-03-24 21:09:55 -0600

    Agregando las tres carpetas faltantes

--- condicionales/Ejercicio_2.py
+++ condicionales/Ejercicio_2.py
@@ -1,9 +1,10 @@
- num_1 =int(input('Dime el numero'))
+Algoritmos para numeros        
+num_1 =int(input('Dime el numero'))
  if num_1== 0:
  printf('Es igual a 0')
  elif num_1 > 0:
  print('Es positivo')
  else:
  if num_1 < 0:(print('Es negativo'))
- print('Fin')
+ print('Fin')  
  
